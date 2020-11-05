c315c762df0b51cd397ecf8c34eca8a0d66797ab
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2020-11-05 21:46:57 +0100

    Add getting arbitrary country stats.

--- start.py
+++ start.py
@@ -73,63 +73,86 @@ def add_region_counts(embed: Embed, counts: list, show_diff: bool, limit: int = 
         )
 
 
+async def covid_setup(ctx: Context, *args):
+    if len(args) == 1:
+        await ctx.send("Usage: `!covid setup <country>`")
+        return
+
+    country = await covid.get_dataset_id(country=args[1])
+    if country is None:
+        await ctx.send(f"The country name `{args[1]}` is invalid.")
+        return
+    (country_name, dataset_id) = country
+
+    database.add_guild(ctx.guild, country_name, dataset_id)
+    await ctx.send(f"The guild is now set up for country `{country_name}`. "
+                   "Try it using `!covid`.\n"
+                   "Set up daily statistics for a channel using `!covid notify`.")
+
+
+async def covid_get(ctx: Context, time_start: float, country_name: str, dataset_id: str):
+    items = await covid.get_items(dataset_id=dataset_id, limit=3, descending=True)
+
+    counts = covid.get_counts(items)
+    diffs = covid.get_diffs(items)
+    counts.reverse()
+    diffs.reverse()
+
+    today_date = diffs[0]["date"]
+
+    embed = Embed(title=today_date.strftime(TEXT_STATS_TODAY_FORMAT))
+    embed.description = TEXT_STATS_DESCRIPTION
+    embed.set_author(
+        name=TEXT_TITLE_TOP,
+        icon_url="https://cdn.discordapp.com/embed/avatars/4.png",
+    )
+
+    add_counts(embed, diffs, show_diff=True)
+    add_region_counts(embed, diffs, show_diff=True, limit=5)
+    embed.add_field(
+        name="\u200b",
+        value=TEXT_STATS_TOTAL,
+        inline=False,
+    )
+    add_counts(embed, counts, show_diff=False)
+    # add_region_counts(embed, counts, show_diff=False, limit=5)
+
+    time_end = time.time() * 1000
+    embed.set_footer(text=f"{int(time_end - time_start)} ms")
+
+    await ctx.send(embed=embed)
+
+
 @bot.command(name="covid")
 async def main(ctx: Context, *args):
     time_start = time.time() * 1000
-    if args and args[0] == "setup":
-        if len(args) == 1:
-            await ctx.send("Usage: `!covid setup <country>`")
-            return
+    try:
+        if args:
+            if args[0] == "setup":
+                await covid_setup(ctx, args)
+                return
+            elif args[0] == "notify":
+                await covid_setup(ctx, args)
+                return
+            else:
+                country = await covid.get_dataset_id(country=args[0])
+                if country is None:
+                    await ctx.send(f"The country name `{args[0]}` is invalid.")
+                    return
+                (country_name, dataset_id) = country
+        else:
+            config = database.get_by_guild(ctx.guild)
+            if not config or not config[0]["datasetId"]:
+                await ctx.send("Configure the guild first using `!covid setup <country>`.")
+                return
+            config = config[0]
+            country_name = config["countryName"]
+            dataset_id = config["datasetId"]
 
-        country = await covid.get_dataset_id(country=args[1])
-        if country is None:
-            await ctx.send(f"The country name `{args[1]}` is invalid.")
-            return
-        (country_name, dataset_id) = country
-
-        database.add_guild(ctx.guild, country_name, dataset_id)
-        await ctx.send(f"The guild is now set up for country `{country_name}`. "
-                       "Try it using `!covid`.\n"
-                       "Set up daily statistics for a channel using `!covid notify`.")
-        return
-
-    config = database.get_by_guild(ctx.guild)
-    if not config or not config[0]["datasetId"]:
-        await ctx.send("Configure the guild first using `!covid setup <country>`.")
-        return
-    config = config[0]
-
-    async with ctx.channel.typing():
-        items = await covid.get_items(dataset_id=config["datasetId"], limit=3, descending=True)
-
-        counts = covid.get_counts(items)
-        diffs = covid.get_diffs(items)
-        counts.reverse()
-        diffs.reverse()
-
-        today_date = diffs[0]["date"]
-
-        embed = Embed(title=today_date.strftime(TEXT_STATS_TODAY_FORMAT))
-        embed.description = TEXT_STATS_DESCRIPTION
-        embed.set_author(
-            name=TEXT_TITLE_TOP,
-            icon_url="https://cdn.discordapp.com/embed/avatars/4.png",
-        )
-
-        add_counts(embed, diffs, show_diff=True)
-        add_region_counts(embed, diffs, show_diff=True, limit=5)
-        embed.add_field(
-            name="\u200b",
-            value=TEXT_STATS_TOTAL,
-            inline=False,
-        )
-        add_counts(embed, counts, show_diff=False)
-        # add_region_counts(embed, counts, show_diff=False, limit=5)
-
-        time_end = time.time() * 1000
-        embed.set_footer(text=f"{int(time_end - time_start)} ms")
-
-        await ctx.send(embed=embed)
+        async with ctx.channel.typing():
+            await covid_get(ctx, time_start, country_name, dataset_id)
+    except Exception as e:
+        await ctx.send(str(e))
 
 
 @bot.event
